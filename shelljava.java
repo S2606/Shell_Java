6d73d553de9d70d39f358b6147fa15855d115d28
Shaggy2606 <shagun.khemka60@gmail.com>
2017-04-08 17:06:52 +0530

    Commit

---
+++ shelljava.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.lang.Runtime;
+import java.util.*;
+import java.net.*;
+public class shelljava
+{
+    public static void main(String args[])throws IOException
+    {
+        try
+        {
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter your git remote url");//Accepting URL
+            String u=br.readLine();
+            URL url=new URL(u);
+            //System.out.println("Enter your commit sentence");//Commit line
+            String commit="make it better";
+            System.out.println("Get ready for your code to be on github in few minutes..");
+            String comd[]=new String[6];
+            comd[0]="git init";
+            comd[1]="git add .";
+            comd[2]="git commit -m "+commit;
+            comd[3]="git remote add origin "+url;
+            comd[4]="git pull";
+            comd[5]="git push -f origin master";
+            for(int i=0;i<4;i++)
+            {
+                String cmd=comd[i];
+                Runtime run = Runtime.getRuntime();
+                Process pr = run.exec(cmd);
+                pr.waitFor();
+                BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+                String line = "";
+                while ((line=buf.readLine())!=null) {
+                    System.out.println(line);
+                }
+            }
+            System.out.println("Uploaded on github..");
+        }catch(InterruptedException e){
+
+        }
+    }
+}
